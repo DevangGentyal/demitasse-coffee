4a564ececf6ce95863bab1aee79442d355b3e699
Avanish <avanishsuryavanshi73@gmail.com>
2026-06-22 23:17:15 +0530

    pwa  setup customer-frontend

--- customer-frontend/src/main.jsx
+++ customer-frontend/src/main.jsx
@@ -3,7 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
+import { registerSW } from "virtual:pwa-register";
 
+registerSW({
+  immediate: true,
+});
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
